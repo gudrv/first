1aab2438823d67c7324c41a498431fcae182c20b
rvgud <>
2017-06-01 04:42:55 +0530

    Twiiter APi added for re tweets

---
+++ Services/TwitterService.php
@@ -0,0 +1,77 @@
+<?phpSSS
+/**
+ * Description of TwitterService
+ *
+ * @author rvgud
+ * Used to interact with Twitter API
+ */
+require_once('External\Twitter\TwitterAPIExchange.php');
+class TwitterService {
+    /**
+     * put your code here
+     * @param String $hashtag value of the hashtag
+     * get all the tweets associated with a hashtag provided
+     */
+    public function getTweetsByHashtag($hashtag)
+    {
+        //url to fetch all the tweets
+        $url = 'https://api.twitter.com/1.1/search/tweets.json';
+        //mentioning the hashtag value
+        $getfield = '?q=#'.$hashtag;
+        //mentioning the request type method
+        $requestMethod = 'GET';
+        //setting the twitter app credentials
+        $settings = array(
+                            'oauth_access_token' => "577935649-IsYosowKwiGIVqQg2zGrhiZLU5dRm6RbL6hyupJr",
+                            'oauth_access_token_secret' => "g5EXmpmU9rKWyF2GtaNaIPciW3c6BiCgHIqihuELCm0tc",
+                            'consumer_key' => "rjOhiPCNZewPjG17XyJ7Mguvb",
+                            'consumer_secret' => "e3Exfa3olWz7YjsaW50NJ4NMpX2jDNZkitn5vnntqFtnMXE4rx"
+                        );
+        $twitter = new TwitterAPIExchange($settings);
+        $data= $twitter->setGetfield($getfield)
+                     ->buildOauth($url, $requestMethod)
+                     ->performRequest();
+        //decode the json data into php variable
+        $data=(array)@json_decode($data, false); 
+        //statuses is having all the tweets
+        return $data['statuses'];
+    }
+     /**
+     * put your code here
+     * @param String $hashtag value of the hashtag
+     * filter the tweets according to their retweet count
+     */
+    public function getReTweetedTweetsByHashtag($hashtag)
+    {
+        try {
+            $tweets=$this->getTweetsByHashtag($hashtag);
+            //in case no tweet found for the hashtag
+            if(empty($tweets)){
+                throw new Exception("No tweet found for the hashtag:- #".$hashtag); 
+            }
+            $retweets=[];
+            //filter the tweets accorig to their retweetcount and save in different array
+            foreach ($tweets as $value) {
+                if($value->retweet_count >0){
+                    //create a new object and set the value required of a tweet.
+                    $tweetdata=new stdClass();
+                    $tweetdata->username=$value->user->name;
+                    $tweetdata->userprofileimageurl=$value->user->profile_image_url_https;
+                    $tweetdata->userscreenname=$value->user->screen_name;
+                    $tweetdata->tweettext=$value->text;
+                    $tweetdata->tweettime=$value->created_at;
+                    array_push($retweets, $tweetdata);
+                }
+            }
+            //in case no tweet is retweeted
+            if(sizeof($retweets) == 0){
+                throw new Exception("No retweeted tweet found for the hashtag:- #".$hashtag);
+            }
+            //return the retweeted tweets
+            return $retweets;
+        } 
+        catch (Exception $ex) {
+            echo $ex->getMessage();
+        }
+    }
+}
---
+++ index.php
@@ -0,0 +1,51 @@
+<?php 
+ /**
+     * Service class s responsible for interaction with Twitter API.
+     */
+require_once("Services\TwitterService.php");
+$tweetservice=new TwitterService();
+$tweets=$tweetservice->getReTweetedTweetsByHashtag("custserv");
+?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="main.css"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container-fluid">
+        <?php
+        foreach ($tweets as $value) {
+            ?>
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <a class="account-group" href="http://twitter.com/<?php echo $value->userscreenname; ?>">
+                            <img class="avatar" src="<?php echo $value->userprofileimageurl; ?>" alt="">
+                            <span class="FullNameGroup">
+                                <strong><?php echo $value->username; ?></strong>
+                                @<?php echo $value->userscreenname; ?> 
+                                at <?php echo $value->tweettime; ?>
+                            </span>
+                        </a>
+                        <p>
+                            <b>#custserv</b>
+                        </p>
+                        <p><?php echo $value->tweettext; ?></p>
+                    </div>
+                </div>
+                
+            <?php
+        }
+        ?>
+       </div>
+        
+        
+    </body>
+</html>
